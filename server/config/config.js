42978ca16725b7792358dac65d12eda6cd00dd88
AlexanderHOB <obispoa1@gmail.com>
2020-10-01 15:39:07 -0500

    Varible de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
-    urlDB='mongodb+srv://user-cafe:z4qTM2XdkrAh5mI8@cluster0.lcqy2.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB=urlDB;
